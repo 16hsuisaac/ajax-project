c6a911fb7a73ab978a57a6b14d0130a31df0339e
Isaac Hsu <16hsuisaac@gmail.com>
2021-07-27 13:30:58 -0700

    added in breed

--- js/main.js
+++ js/main.js
@@ -3,10 +3,12 @@ var $openingScreen = document.querySelector('.opening-screen');
 var $dog = document.querySelector('.dog');
 var $body = document.querySelector('body');
 var $homeScreen = document.querySelector('.home-screen');
-
-var xhr = getDogPic();
+var $dogBreedText = document.querySelector('.dog-breed-text');
+var breed = null;
 
 $getStarted.addEventListener('click', openApp);
+
+var xhr = getDogPic();
 
 function openApp(event) {
   $openingScreen.setAttribute('class', 'opening-screen row container hidden');
@@ -20,6 +22,16 @@ function getDogPic() {
   xhr['x-api-key'] = '9c73ad28-2006-43c9-8f08-9e2c96fc540a';
   xhr.open('GET', 'https://api.thedogapi.com/v1/images/search');
   xhr.responseType = 'json';
+  xhr.addEventListener('load', breedName);
   xhr.send();
   return xhr;
+}
+
+function breedName(event) {
+  if (xhr.response[0].breeds[0]) {
+    breed = xhr.response[0].breeds[0].name;
+  } else {
+    breed = 'Unknown';
+  }
+  $dogBreedText.textContent = breed;
 }
